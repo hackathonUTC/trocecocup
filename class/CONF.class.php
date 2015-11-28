24bf6e50b508a5d07da218bdc68db36d045cad9a
Vinceguit <vince.baheux@gmail.com>
2015-11-28 18:15:24 +0100

    Navbar config

---
+++ class/CONF.class.php
@@ -0,0 +1,12 @@
+<?php
+class CONF
+{
+    public static function activeLink($navbar, $page)
+    {
+      if(isset($navbar) AND isset($page))
+      {
+  			if($navbar == $page)
+  				echo 'class="active"'; 
+  		}
+    }
+}
--- view/index.php
+++ view/index.php
@@ -1,11 +1,32 @@
+<?php $nomProjet = 'Eco Troc'; ?>
 <!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8"/>
-  <title>EcoTroc</title>
-</head>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title><?php echo $nomProjet; ?></title>
 
-<body>
-  <h1>Ca marche !</h1>
-</body>
+    <!-- Bootstrap -->
+    <link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <?php
+      $navbar = 'catalogue';
+      include('view/nav.php');
+    ?>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="./bootstrap/js/bootstrap.min.js"></script>
+  </body>
 </html>
