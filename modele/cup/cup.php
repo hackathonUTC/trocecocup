4581f06c60ddd168a57933b2f07127b5758a2b8a
oisive <lea.ceccarelli@gmail.com>
2015-11-29 02:56:58 +0100

    Merge branch 'master' of https://github.com/hackathonUTC/trocecocup
    # Conflicts:
    #	config.php
    #	index.php

--- modele/cup/cup.php
+++ modele/cup/cup.php
@@ -18,5 +18,18 @@ function getAllAsso(){
 	else return null;
 } 
 
+function aMoi($log){
+	global $mysqli;
+	$query = "SELECT DISTINCT Cup.cup 
+			  FROM Cup
+			  LEFT JOIN liste_cup on liste_cup.cup = Cup.cup
+			  LEFT JOIN liste_user on liste_user.liste = liste_cup.liste
+			  WHERE liste_user.login = $log";
+	if ($result = mysqli_query($mysqli, $query)) {
+		return $result;
+	}
+	else return null;
+}
+
 
 ?>
--- vue/cup/cup.php
+++ vue/cup/cup.php
@@ -1,7 +1,7 @@
 <div id="grid" class="row-fluid">
 
 	<span>Filter :</span> 
-	<div class="dropdown">
+	<div class="dropdown sort-options">
   <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
     Asso
     <span class="caret"></span>
@@ -18,40 +18,3 @@
 	?>
 
 </div>
-
-<script>
-$(document).ready(function() {
-  var $grid = $('#grid'),
-      $sizer = $grid.find('.shuffle__sizer');
-
-  $grid.shuffle({
-    itemSelector: '.picture-item',
-    sizer: $sizer
-  });
-});
-
-// Sorting options
-$('.sort-options').on('change', function() {
-  var sort = this.value,
-      opts = {};
-
-  // We're given the element wrapped in jQuery
-  if ( sort === 'date-created' ) {
-    opts = {
-      reverse: true,
-      by: function($el) {
-        return $el.data('date-created');
-      }
-    };
-  } else if ( sort === 'title' ) {
-    opts = {
-      by: function($el) {
-        return $el.data('title').toLowerCase();
-      }
-    };
-  }
-
-  // Filter elements
-  $grid.shuffle('sort', opts);
-});
-</script>
