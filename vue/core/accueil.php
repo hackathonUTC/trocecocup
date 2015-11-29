c86ceb38eccae39174e98dc28b897efc34112a44
Robin Jolliet <m1105104symphonie@gmail.com>
2015-11-29 05:45:58 +0100

    dfsqfd

--- vue/core/accueil.php
+++ vue/core/accueil.php
@@ -4,15 +4,31 @@
   	include('vue/core/nav.php');
 ?>
 
-<body>
 
-<!-- 
-<div class="container headline">
+<?php
+include('vue/core/header.php');
+$navbar = 'catalogue';
+include('vue/core/nav.php');
+?>
+
+<div class="container headline cataligue">
+	<div class="container headline-inside catalogue-inside">
+		<div class="container headline-inside catalogue-stat">
+			<?php include("vue/core/stats-dashboard.php")?>
+		</div>
+		<div class="container headline-inside catalogue-cup">
+			<?php include("vue/core/ecocups-dashboard.php")?>
+		</div>
+	</div>
 </div>
-<div class="container">
-</div>
+
+<?php
+include('vue/core/footer.php');
+?>
+
+
+
 
 <?php
 	include('vue/core/footer.php');
 ?>
- -->
