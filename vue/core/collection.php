05114ef6ec4ce74e3eca73ce16b2465c975f98fa
Vinceguit <vince.baheux@gmail.com>
2015-11-29 03:14:29 +0100

    Fix de collection.php

--- vue/core/collection.php
+++ vue/core/collection.php
@@ -1,26 +1,8 @@
 <?php
-  $nomProjet = 'SwitchCup';
-  include_once "../config.php";
-  include_once "../class/CONF.class.php";
+  include('vue/core/header.php');
+  $navbar = 'collection';
+  include('nav.php');
 ?>
-<!DOCTYPE html>
-<html lang="fr">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Ma collection</title>
-
-    <!-- Bootstrap -->
-    <link href="<?php echo $_CONF['$accessPath'] .'bootstrap/css/bootstrap.min.css'; ?>" rel="stylesheet">
-  </head>
-  <body>
-
-    <?php
-      $navbar = 'collection';
-      include('nav.php');
-    ?>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
