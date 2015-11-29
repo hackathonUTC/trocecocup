9149e60fcc2d39e7d5675c1c0ae987c76a380030
Vinceguit <vince.baheux@gmail.com>
2015-11-29 01:39:58 +0100

    Added 404 error

---
+++ vue/core/404.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>SwitchCup</title>
+
+    <!-- Bootstrap -->
+    <link href="includes/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="includes/app.css" rel="stylesheet">
+  </head>
+  <body>
+    <?php
+      include('vue/core/nav.php');
+    ?>
+
+    <div class="headline">
+      <div class="container">
+        <h1>Oups !</h1>
+        <p>La page que vous avez demandé n'existe pas. :(</p>
+      </div>
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="includes/jquery/js/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="includes/bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
