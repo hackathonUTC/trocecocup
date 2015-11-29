df4ad61ee99ba993b88ee7154daa72947be7a29b
Benihoud Khalil <khalil.benihoud@gmail.com>
2015-11-29 03:37:51 +0100

    fixed footer

--- vue/core/footer.php
+++ vue/core/footer.php
@@ -1,3 +1,9 @@
 <footer>
 <p>Cooked with <span class="glyphicon glyphicon-heart" aria-hidden="true" style="color:red;"></span> by the amazing <strong>SwitchCup</strong> team !</p>
 </footer>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="includes/jquery/js/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="includes/bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
