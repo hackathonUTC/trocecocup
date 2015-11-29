b9bb0a18c5b4c25fa56df581deffbc30870d332b
Benihoud Khalil <khalil.benihoud@gmail.com>
2015-11-29 04:35:23 +0100

    fixed css bugs

--- vue/core/index.php
+++ vue/core/index.php
@@ -9,24 +9,14 @@
         <h1>Switchez vos <span>eco-cups</span> facilement</h1>
         <p>Sans prise de tête ni attente..</p>
     </div>
-<<<<<<< HEAD
-    <div class="container">
-        <div class="box row">
-            <img src="./includes/img/illustration-switch.png" alt="Illustration" />
-            <h2>Facilité d’échange. Plateforme simple à utiliser. Just do it. </h2>
-            <p>Echange ton eco-cup en quelques clics...</p>
-<!--             <button type="button" class="btn btn-default">Connexion CAS</button>
- -->          </div>
-        </div>
-=======
+
 </div>
 <div class="container">
     <div class="box row">
         <img src="./includes/img/illustration-switch.png" alt="Illustration" />
-        <h2>Facilité d’échange. Lorem Ipsum. Lorem Ipsum. </h2>
-        <p>Sans prise de tête ni attente..</p><br/>
-        <button type="button" class="btn btn-default">Connexion CAS</button>
->>>>>>> bc59e858cc61fbddb97db3a271d224132a8b0cae
+            <h2>Facilité d’échange. Plateforme simple à utiliser. Just do it. </h2>
+            <p>Echange ton eco-cup en quelques clics...</p>
+       <!--  <button type="button" class="btn btn-default">Connexion CAS</button> -->
     </div>
 </div>
 <?php
